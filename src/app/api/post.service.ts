c82853d5579c1647016dd40e439137bbe5ab2d4a
maxsoulard <max.soulard@gmail.com>
2017-10-18 13:28:45 +0200

    Do not display detailed post if selftext is empty.

--- src/app/api/post.service.ts
+++ src/app/api/post.service.ts
@@ -19,7 +19,7 @@ export class PostService {
     isDisplayable(post: any) {
         const redditHostSlashRSlash = `${this.apiConstantes.baseUrl}${this.apiConstantes.slashRSlash}`;
         const postHintIsImage = post.data.post_hint === 'image';
-        const urlHostIsReddit = post.data.url.indexOf(redditHostSlashRSlash) > -1;
+        const urlHostIsReddit = post.data.url.indexOf(redditHostSlashRSlash) > -1 && post.data.selftext_html;
         const urlHostIsTheSameSubreddit = post.data.url.indexOf(`${redditHostSlashRSlash}${post.data.subreddit}`) > -1; // TODO handle comments link
         return postHintIsImage || (urlHostIsReddit && urlHostIsTheSameSubreddit);
     }
