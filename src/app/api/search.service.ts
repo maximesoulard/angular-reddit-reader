fe147110934b5f28d68d2afca908d33da6417755
maxsoulard <max.soulard@gmail.com>
2017-10-20 15:39:51 +0200

    Search subreddit service

--- src/app/api/search.service.ts
+++ src/app/api/search.service.ts
@@ -1,13 +1,26 @@
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Http, Response } from '@angular/http';
 import { ApiConstantes } from './api.constantes';
+
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchService {
-    constructor(private httpClient: HttpClient, private apiConstantes: ApiConstantes) { }
+    constructor(private http: Http, private apiConstantes: ApiConstantes) { }
     
-    searchForASubreddit(q: string, sort: string) {
+    searchForASubreddit(q: string, sort: string): Observable<string[]> {
         sort = sort !== null ? sort : 'relevance';
-        return this.httpClient.get(`${this.apiConstantes.search}?q=${q}&sort=${sort}`);
+        return this.http
+            .get(`${this.apiConstantes.search}?q=${q}&sort=${sort}`)
+            .map(res => res.json())
+            .map(((response: any) => {
+                const subreddits = response.data.children.map(child => {
+                    return child.data.display_name;
+                });
+                return subreddits;
+            }));
     }
 }
